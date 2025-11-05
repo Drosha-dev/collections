aedbe1ef9b882b0e62cef9e0fe9a8f04a17a724d
Alex Thanas <greek313@gmail.com>
2025-11-05 13:38:42 -0600

    Added error component, footer, and CSS updates

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import './App.css'
 import Note from '../components/note.jsx'
 import { useState, useEffect } from 'react'
 import noteService from "./services/notes.js"
+import Notification from '../components/error.jsx'
+import Footer from '../components/Footer.jsx'
 
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage]= useState('some error happened')
 
   useEffect(() => {
 
@@ -24,8 +27,18 @@ const App = () => {
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
 
-    noteService.update(id,changedNote).then(returnedNote => {
+    noteService.update(id,changedNote)
+    .then(returnedNote => {
       setNotes(notes.map(note => note.id === id ? returnedNote : note))
+    })
+    .catch(error => {
+      setErrorMessage(
+        `Note '${note.content}' was already removed from the server ${error}`
+        
+      )
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000).setNotes(notes.filter(n => n.id !== id))
     })
   }
 
@@ -48,12 +61,13 @@ const App = () => {
 
   }
 
-
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
 
   return (
     <>
+    <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage}/>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -70,6 +84,8 @@ const App = () => {
         />
         <button type='submit'>save</button>
       </form>
+      <Footer />
+      </div>
     </>
   )
 }
